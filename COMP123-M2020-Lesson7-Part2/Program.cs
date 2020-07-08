fe9c02b4f893e078fa12a255566043114dbf5dba
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2020-07-08 12:23:59 -0400

    Add project files.

---
+++ COMP123-M2020-Lesson7-Part2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace COMP123_M2020_Lesson7_Part2
+{
+    class Program
+    {
+        static void waitForAnyKey()
+        {
+            Console.ReadKey();
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Vector2D position = new Vector2D();
+            float Xinput, Yinput;
+            bool XInputCorrect = false;
+            bool YInputCorrect = false;
+
+            while (!XInputCorrect)
+            {
+                Console.Write("Enter the X coordinate: ");
+                if (float.TryParse(Console.ReadLine(), out Xinput))
+                {
+                    position.x = Xinput;
+                    XInputCorrect = true;
+                }
+                else
+                {
+                    Console.WriteLine("------------------------------------");
+                    Console.WriteLine("ERROR");
+                    Console.WriteLine("Please enter a number value for X");
+                    Console.WriteLine("------------------------------------");
+                    Console.WriteLine();
+                }
+            }
+
+            while (!YInputCorrect)
+            {
+                Console.Write("Enter the Y coordinate: ");
+                if (float.TryParse(Console.ReadLine(), out Yinput))
+                {
+                    position.y = Yinput;
+                    YInputCorrect = true;
+                }
+                else
+                {
+                    Console.WriteLine("------------------------------------");
+                    Console.WriteLine("ERROR");
+                    Console.WriteLine("Please enter a number value for Y");
+                    Console.WriteLine("------------------------------------");
+                    Console.WriteLine();
+                }
+            }
+
+            // encoding for file output
+            Console.WriteLine();
+            Console.WriteLine("========================================");
+            Console.WriteLine("Writing to File");
+            Console.WriteLine("========================================");
+            Console.WriteLine();
+            // Steps for Writing Files
+            // Step 1 - setup our streams
+            FileStream outPutFile = new FileStream("positions.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(outPutFile);
+
+            // Step 2 - Write to the stream
+            writer.WriteLine(position.StringEncode());
+
+            // Step 3 - clean up
+            writer.Close();
+            outPutFile.Close();
+
+            // decoding for file input
+            Console.WriteLine();
+            Console.WriteLine("========================================");
+            Console.WriteLine("Reading from File");
+            Console.WriteLine("========================================");
+            Console.WriteLine();
+            // Steps for Reading Files
+            // Step 1 - setup our streams
+            FileStream inputFile = new FileStream("positions.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(inputFile);
+
+            // Step 2 - Read from the stream
+            string inputString = reader.ReadLine();
+
+            Console.WriteLine("String read from file: " + inputString);
+
+            // Step 3 - clean up
+            reader.Close();
+            inputFile.Close();
+
+
+            Console.WriteLine("Decoding Coordinates");
+            position.StringDecode(inputString);
+
+            Console.WriteLine("New Coordinates are: " + position.ToString());
+
+
+            waitForAnyKey();
+        }
+    }
+}
---
+++ COMP123-M2020-Lesson7-Part2/Vector2D.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP123_M2020_Lesson7_Part2
+{
+    public class Vector2D
+    {
+        // PRIVATE INSTANCE VARIABLES (FIELDS)
+        private float m_x;
+        private float m_y;
+
+        // CONSTRUCTOR
+        public Vector2D(float x = 0, float y = 0)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        // PUBLIC PROPERTIES
+        public float x
+        {
+            get
+            {
+                return this.m_x;
+            }
+
+            set
+            {
+                this.m_x = value;
+            }
+        }
+
+        public float y
+        {
+            get
+            {
+                return this.m_y;
+            }
+
+            set
+            {
+                this.m_y = value;
+            }
+        }
+
+        /// <summary>
+        /// This method overrides the built-in ToString method
+        /// It returns a string that is formatted as (x, y)
+        /// where x and y are the property values
+        /// </summary>
+        /// <returns>string</returns>
+        public override string ToString()
+        {
+            return "(" + this.x + ", " + this.y + ")";
+        }
+
+        public string StringEncode()
+        {
+            return this.x + "," + this.y;
+        }
+
+        public void StringDecode(string codedString)
+        {
+            string[] coordinates = codedString.Split(',');
+
+            this.x = Convert.ToSingle(coordinates[0]);
+            this.y = Convert.ToSingle(coordinates[1]);
+        }
+    }
+}
